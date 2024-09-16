2d0afc939bb9c54ee51ce060822eb2a206cfdbff
dhyasuda <dhyasuda0@gmail.com>
2024-09-16 22:47:52 +0100

    assignment in class and hw1

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,19 +1,28 @@
-
-
-  extern int __bss_start, __bss_end;
+extern int __bss_start, __bss_end;
   
-  void clearBss(void){
-  
+   unsigned long get_timer_count(){
+	unsigned long *timer_count_register = (unsigned long*) 0x3f003004;
+	return *timer_count_register;
+}
+ 	
+ void clearBss(void){ 
   char *bss = &__bss_start;
   
   while(bss < &__bss_end){
   *bss++ == 0;
-}
+	}
 
-}
+	}
 void kernel_main() {
-
     clearBss();
+    //timer
+    unsigned long start = get_timer_count();
+    unsigned long wait = 1000;
+    int x = 1234;
+    	
+    while(get_timer_count() - start < wait ){
+	
+    }
     while(1){
     }
 }
