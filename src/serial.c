5d13dc1a205bf247b9b8a2ac930b37a84a24e809
DHYASUDA <58985757+DHYASUDA@users.noreply.github.com>
2024-10-02 22:28:56 -0500

    Update serial.c

--- src/serial.c
+++ src/serial.c
@@ -1,26 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
 #include "rprintf.h"
 
-#define SERIAL_PORT_ADDR 0xFE215040
-
-// Memory-mapped I/O write
-static inline void MMIO_WRITE(unsigned int reg, unsigned int data) {
-    *(volatile unsigned int *)reg = data;
-}
-
-// Memory-mapped I/O read
-static inline unsigned int MMIO_READ(unsigned int reg) {
-    return *(volatile unsigned int *)reg;
-}
+// Memory-mapped I/O macros
+#define SERIAL_PORT_ADDR 0x7D001000
+#define MMIO_WRITE(reg, data) (*(volatile uint32_t *)(uintptr_t)(reg) = (data))
+#define MMIO_READ(reg) (*(volatile uint32_t *)(uintptr_t)(reg))
 
 // Function to write a character to the serial port
-void putc(int data) {
+int my_putc(int data) {
+    printf("Testing\n");
+
     // Wait until the serial port is ready to transmit (checking the status register)
-    while (!(MMIO_READ(SERIAL_PORT_ADDR + 0x18) & 0x20)) {
-        // Busy-wait until the transmit FIFO has space
+    while (!(MMIO_READ(SERIAL_PORT_ADDR + 0x18) & 0x20)) { 
+        // Busy wait
     }
 
     // Write the character to the serial data register
     MMIO_WRITE(SERIAL_PORT_ADDR, data);
+    return 0;
 }
 
 // Function to get the current execution level
@@ -29,11 +27,18 @@ unsigned int getEL() {
     // Inline assembly to read the CurrentEL register
     asm volatile ("mrs %0, CurrentEL" : "=r" (el));
     
-    // The EL is stored in bits [3:2] of the CurrentEL register, so we shift right by 2 to get the actual value
+    // The EL is stored in bits [3:2] of the CurrentEL register, so we shift right
     return (el >> 2) & 0x3;
 }
 
 // Test function to demonstrate printing the current execution level using esp_printf
 void print_execution_level() {
-    esp_printf(putc, "Current Execution Level is %d\r\n", getEL());
+    esp_printf(my_putc, "Current Execution Level is %d\r\n", getEL());
+}
+
+// Main function
+int main() {
+    my_putc('A');
+    print_execution_level();
+    return 0;
 }
