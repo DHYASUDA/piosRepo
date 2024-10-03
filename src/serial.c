aea0ae800f9fb2f2c71494e669cd42c537c9a4f6
DHYASUDA <58985757+DHYASUDA@users.noreply.github.com>
2024-10-02 23:31:44 -0500

    Update serial.c

--- src/serial.c
+++ src/serial.c
@@ -1,44 +1,38 @@
-#include <stdio.h>
 #include <stdint.h>
 #include "rprintf.h"
-
-// Memory-mapped I/O macros
+#include "rprintf.c"
 #define SERIAL_PORT_ADDR 0x7D001000
-#define MMIO_WRITE(reg, data) (*(volatile uint32_t *)(uintptr_t)(reg) = (data))
-#define MMIO_READ(reg) (*(volatile uint32_t *)(uintptr_t)(reg))
 
-// Function to write a character to the serial port
-int my_putc(int data) {
-    printf("Testing\n");
-
-    // Wait until the serial port is ready to transmit (checking the status register)
-    while (!(MMIO_READ(SERIAL_PORT_ADDR + 0x18) & 0x20)) { 
-        // Busy wait
-    }
-
-    // Write the character to the serial data register
-    MMIO_WRITE(SERIAL_PORT_ADDR, data);
-    return 0;
+static inline uint32_t MMIO_READ(uint32_t addr) {
+            return *(volatile uint32_t *)(uintptr_t)addr;
 }
 
-// Function to get the current execution level
-unsigned int getEL() {
-    unsigned int el;
-    // Inline assembly to read the CurrentEL register
-    asm volatile ("mrs %0, CurrentEL" : "=r" (el));
-    
-    // The EL is stored in bits [3:2] of the CurrentEL register, so we shift right
-    return (el >> 2) & 0x3;
+static inline void MMIO_WRITE(uint32_t addr, uint32_t data) {
+            *(volatile uint32_t *)(uintptr_t)addr = data;
 }
 
-// Test function to demonstrate printing the current execution level using esp_printf
-void print_execution_level() {
-    esp_printf(my_putc, "Current Execution Level is %d\r\n", getEL());
+int s_putc(int data) {
+            if (MMIO_READ(SERIAL_PORT_ADDR + 0x18) & 0x20) {
+                            MMIO_WRITE(SERIAL_PORT_ADDR, data);
+                            return data;
+            }
+               return -1;
 }
 
-// Main function
-int main() {
-    my_putc('A');
-    print_execution_level();
-    return 0;
+
+int getEL() {
+            unsigned int el;
+           asm volatile ("mrs %0, CurrentEL" : "=r" (el));
+          return (el >> 2) & 0x3;
 }
+void print_execution_level(){
+   esp_printf(s_putc,"level : %d\r\n", getEL());
+
+}
+
+int  main() {
+            int current_level = getEL();
+                print_execution_level(current_level);
+                return 0;
+}
+
